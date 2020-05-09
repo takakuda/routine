3521d0f0d720378494d28448c8745ddb6e74e74e
kei <kei.takakuda.0415@icloud.com>
2020-05-09 10:53:47 +0900

    simple slice

---
+++ go/0509/simple_slicing.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  a := [5]int{1, 2, 3, 4, 5}
+  s := a[0:2]
+  fmt.Println(s)
+  t := a[2:]
+  fmt.Println(t)
+
+  b := [...]int{1, 2, 3}
+  fmt.Println(len(b))
+  fmt.Println(cap(b))
+  // invalid slice index 4 (out of bounds for 3-element array)
+  // fmt.Println(b[0:4])
+}
+
