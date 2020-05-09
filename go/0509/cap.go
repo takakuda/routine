770a5fde6fb702fd6d15a49f5ec35529473bc999
kei <kei.takakuda.0415@icloud.com>
2020-05-09 10:46:10 +0900

    cap

---
+++ go/0509/cap.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  s := make([]int, 4)
+  fmt.Println(len(s))
+  fmt.Println(cap(s))
+
+  t := make([]int, 5, 10)
+  fmt.Println(len(t))
+  fmt.Println(cap(t))
+}
