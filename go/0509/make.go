71278c2d4cbe16c10575a2a7f05d97def7844fc1
kei <kei.takakuda.0415@icloud.com>
2020-05-09 10:36:57 +0900

    assignments to elements

--- go/0509/make.go
+++ go/0509/make.go
@@ -7,4 +7,14 @@ import (
 func main() {
   s := make([]int, 10)
   fmt.Println(s)
+
+  a := make([]float64, 3)
+  a[0] = 3.14
+  fmt.Println(a)
+
+  a[1] = 6.14
+  fmt.Println(a)
+
+  // panic: runtime error: index out of range [4] with length 3
+  // fmt.Println(a[4])
 }
