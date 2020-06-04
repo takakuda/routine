1b0599c59e58285a1b4ef27ecf0b32851f56a581
takakuda <kei.takakuda.0415@icloud.com>
2020-06-05 05:07:01 +0900

    new

--- go/struct.go
+++ go/struct.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Animal struct {
 	Name string
 	Age  int
@@ -8,4 +12,16 @@ type Animal struct {
 type Cat struct {
 	Animal
 	ServanName string
+}
+
+func main() {
+	c := Cat{}
+	c.Name = "taro"
+	fmt.Println(c.Name)
+
+	a := &Animal{Name: "tako", Age: 1}
+	fmt.Println(a)
+
+	b := new(Animal)
+	fmt.Println(b)
 }
