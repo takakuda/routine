2c494cfcae15e51b7566a8ca0756bd909585af93
takakuda <kei.takakuda.0415@icloud.com>
2020-09-16 22:59:07 +0900

    write header

--- go/0916/handler.go
+++ go/0916/handler.go
@@ -9,10 +9,13 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "GET hello!\n")
 	case http.MethodPost:
+		w.WriteHeader(http.StatusCreated)
 		fmt.Fprintf(w, "POST hello!\n")
 	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintf(w, "Method not allowed.\n")
 	}
 }
